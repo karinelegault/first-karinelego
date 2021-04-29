ee08b4c8bd9d21e7536df11a2d4c85280bee4f70
Karine Legault <karine.legault.mallette14@gmail.com>
2021-04-29 17:00:32 -0400

    new pages in menu

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: 'about'
   get 'contact', to: 'pages#contact', as: 'contact'
+  get 'work', to: 'pages#work', as: 'work'
+  get 'blog', to: 'pages#blog', as: 'blog'
 end
